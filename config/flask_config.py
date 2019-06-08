7c1851949cafe0cd1660db7d5e04aa87e7d89580
Cris7777 <sichen2019@u.northwestern.edu>
2019-06-08 15:49:57 -0500

    update on flask_config.py

--- config/flask_config.py
+++ config/flask_config.py
@@ -17,4 +17,6 @@ password = os.environ.get("MYSQL_PASSWORD")
 host = os.environ.get("MYSQL_HOST")
 port = os.environ.get("MYSQL_PORT")
 DATABASE_NAME = 'msia423'
-SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.format(conn_type=conn_type, user=user, password=password, host=host, port=port, DATABASE_NAME=DATABASE_NAME)
+
+if SQLALCHEMY_DATABASE_URI is not None:
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.format(conn_type=conn_type, user=user, password=password, host=host, port=port, DATABASE_NAME=DATABASE_NAME)
